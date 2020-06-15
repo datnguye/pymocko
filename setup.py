43239a671fa8de02c18a451dfba760d93ff3a466
ssdavid <david@siliconstack.com.au>
2020-06-15 14:53:25 +0700

    no message

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
      name='pymocko',
-     version='0.1.0',
+     version='0.1.1',
      author='datnguye',
      author_email='datnguyen.it09@gmail.com',
      packages=['pymocko'],
